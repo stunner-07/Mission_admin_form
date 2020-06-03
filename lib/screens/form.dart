df0ef43b89f4717604744612eaf66587ddb129bf
Kshitij Jain <jainkshitij99@gmail.com>
2020-06-04 02:31:15 +0530

    UI upgraded

--- lib/screens/form.dart
+++ lib/screens/form.dart
@@ -7,171 +7,203 @@ import 'package:provider/provider.dart';
 
 import 'microtasks.dart';
 
-class FormScreen extends StatelessWidget {
+class FormScreen extends StatefulWidget {
+  @override
+  _FormScreenState createState() => _FormScreenState();
+}
+
+class _FormScreenState extends State<FormScreen> {
   var mission = Missions();
+
+  bool isLoading = false;
+
   final _formKey = GlobalKey<FormState>();
+
   Future<void> _save(BuildContext context) async {
     _formKey.currentState.save();
-    await Provider.of<MissionProvider>(context,listen: false).createMission(mission);
-    _formKey.currentState.reset();
+    setState(() {
+      isLoading = true;
+      //_formKey.currentState.reset();
+    });
+    await Provider.of<MissionProvider>(context, listen: false)
+        .createMission(mission);
+    
+    // setState(() {
+    //   isLoading = false;
+    // });
+    Navigator.of(context).pushNamed(MicrotaskScreen.routeName);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: 500,
-        height: 700,
-        padding: const EdgeInsets.all(15),
-        child: Form(
-          key: _formKey,
-          child: ListView(
-            children: <Widget>[
-              TextFormField(
-                decoration: InputDecoration(labelText: "CompanyID"),
-                textInputAction: TextInputAction.next,
-                onSaved: (value) {
-                  mission = Missions(
-                    companyId: value,
-                    coverImg: mission.coverImg,
-                    difficulty: mission.difficulty,
-                    microtasksNos: mission.microtasksNos,
-                    name: mission.name,
-                    tags: mission.tags,
-                    details: mission.details,
-                  );
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "CoverImage"),
-                textInputAction: TextInputAction.next,
-                onSaved: (value) {
-                  mission = Missions(
-                    companyId: mission.companyId,
-                    coverImg: value,
-                    difficulty: mission.difficulty,
-                    microtasksNos: mission.microtasksNos,
-                    name: mission.name,
-                    tags: mission.tags,
-                    details: mission.details,
-                  );
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "difficulty"),
-                textInputAction: TextInputAction.next,
-                onSaved: (value) {
-                  mission = Missions(
-                    companyId: mission.companyId,
-                    coverImg: mission.coverImg,
-                    difficulty: value,
-                    microtasksNos: mission.microtasksNos,
-                    name: mission.name,
-                    tags: mission.tags,
-                    details: mission.details,
-                  );
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "No of Microtasks"),
-                textInputAction: TextInputAction.next,
-                onSaved: (value) {
-                  mission = Missions(
-                    companyId: mission.companyId,
-                    coverImg: mission.coverImg,
-                    difficulty: mission.difficulty,
-                    microtasksNos: int.parse(value),
-                    name: mission.name,
-                    tags: mission.tags,
-                    details: mission.details,
-                  );
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "Name"),
-                textInputAction: TextInputAction.next,
-                onSaved: (value) {
-                  mission = Missions(
-                    companyId: mission.companyId,
-                    coverImg: mission.coverImg,
-                    difficulty: mission.difficulty,
-                    microtasksNos: mission.microtasksNos,
-                    name: value,
-                    tags: mission.tags,
-                    details: mission.details,
-                  );
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "tags"),
-                textInputAction: TextInputAction.next,
-                onSaved: (value) {
-                  var lt = new List<String>();
-                  final _delimiter = ',';
-                  final _values = value.split(_delimiter);
-                  //print(_values);
-                  _values.forEach((item) {
-                    lt.add(item);
-                  });
-                  mission = Missions(
-                    companyId: mission.companyId,
-                    coverImg: mission.coverImg,
-                    difficulty: mission.difficulty,
-                    microtasksNos: mission.microtasksNos,
-                    name: mission.name,
-                    tags: lt,
-                    details: mission.details,
-                  );
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "Details Of Mission"),
-                textInputAction: TextInputAction.next,
-                keyboardType: TextInputType.multiline,
-                maxLines: 3,
-                onSaved: (value) {
-                  mission = Missions(
-                    companyId: mission.companyId,
-                    coverImg: mission.coverImg,
-                    difficulty: mission.difficulty,
-                    microtasksNos: mission.microtasksNos,
-                    name: mission.name,
-                    tags: mission.tags,
-                    details: value,
-                  );
-                },
-              ),
-              // RaisedButton(
-              //   onPressed: () => _save(),
-              // ),
-              RaisedButton(
-                onPressed: () async {
-                  await _save(context);
-                  // return showDialog(
-                  //   context: context,
-                  //   builder: (BuildContext context) {
-                  //     return Dialog(
-                  //       shape: RoundedRectangleBorder(
-                  //           borderRadius: BorderRadius.circular(20.0)),
-                  //       child: Container(
-                  //         width: 800,
-                  //         height: 800,
-                  //         child: Padding(
-                  //           padding: const EdgeInsets.all(12.0),
-                  //           child: MicrotaskScreen(mission.missionId),
-                  //         ),
-                  //       ),
-                  //     );
-                  //   },
-                  // );
-                  Navigator.of(context).pushNamed(MicrotaskScreen.routeName);
-                },
-                child: Text('Create Mission AND add microtask'),
-              ),
-            ],
-          ),
-        ),
+      appBar: AppBar(
+        leading: Container(),
       ),
+      body: isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Center(
+              child: Card(
+                elevation: 10,
+                child: Container(
+                  alignment: Alignment.center,
+                  width: 500,
+                  height: 550,
+                  padding: const EdgeInsets.all(15),
+                  child: Form(
+                    key: _formKey,
+                    child: ListView(
+                      children: <Widget>[
+                        TextFormField(
+                          decoration: InputDecoration(labelText: "CompanyID"),
+                          textInputAction: TextInputAction.next,
+                          onSaved: (value) {
+                            mission = Missions(
+                              companyId: value,
+                              coverImg: mission.coverImg,
+                              difficulty: mission.difficulty,
+                              microtasksNos: mission.microtasksNos,
+                              name: mission.name,
+                              tags: mission.tags,
+                              details: mission.details,
+                            );
+                          },
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(labelText: "CoverImage"),
+                          textInputAction: TextInputAction.next,
+                          onSaved: (value) {
+                            mission = Missions(
+                              companyId: mission.companyId,
+                              coverImg: value,
+                              difficulty: mission.difficulty,
+                              microtasksNos: mission.microtasksNos,
+                              name: mission.name,
+                              tags: mission.tags,
+                              details: mission.details,
+                            );
+                          },
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(labelText: "Difficulty"),
+                          textInputAction: TextInputAction.next,
+                          onSaved: (value) {
+                            mission = Missions(
+                              companyId: mission.companyId,
+                              coverImg: mission.coverImg,
+                              difficulty: value,
+                              microtasksNos: mission.microtasksNos,
+                              name: mission.name,
+                              tags: mission.tags,
+                              details: mission.details,
+                            );
+                          },
+                        ),
+                        TextFormField(
+                          decoration:
+                              InputDecoration(labelText: "No of Microtasks"),
+                          textInputAction: TextInputAction.next,
+                          onSaved: (value) {
+                            mission = Missions(
+                              companyId: mission.companyId,
+                              coverImg: mission.coverImg,
+                              difficulty: mission.difficulty,
+                              microtasksNos: int.parse(value),
+                              name: mission.name,
+                              tags: mission.tags,
+                              details: mission.details,
+                            );
+                          },
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(labelText: "Name"),
+                          textInputAction: TextInputAction.next,
+                          onSaved: (value) {
+                            mission = Missions(
+                              companyId: mission.companyId,
+                              coverImg: mission.coverImg,
+                              difficulty: mission.difficulty,
+                              microtasksNos: mission.microtasksNos,
+                              name: value,
+                              tags: mission.tags,
+                              details: mission.details,
+                            );
+                          },
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(labelText: "Tags"),
+                          textInputAction: TextInputAction.next,
+                          onSaved: (value) {
+                            var lt = new List<String>();
+                            final _delimiter = ',';
+                            final _values = value.split(_delimiter);
+                            //print(_values);
+                            _values.forEach((item) {
+                              lt.add(item);
+                            });
+                            mission = Missions(
+                              companyId: mission.companyId,
+                              coverImg: mission.coverImg,
+                              difficulty: mission.difficulty,
+                              microtasksNos: mission.microtasksNos,
+                              name: mission.name,
+                              tags: lt,
+                              details: mission.details,
+                            );
+                          },
+                        ),
+                        TextFormField(
+                          decoration:
+                              InputDecoration(labelText: "Details Of Mission"),
+                          textInputAction: TextInputAction.next,
+                          keyboardType: TextInputType.multiline,
+                          maxLines: 3,
+                          onSaved: (value) {
+                            mission = Missions(
+                              companyId: mission.companyId,
+                              coverImg: mission.coverImg,
+                              difficulty: mission.difficulty,
+                              microtasksNos: mission.microtasksNos,
+                              name: mission.name,
+                              tags: mission.tags,
+                              details: value,
+                            );
+                          },
+                        ),
+                        // RaisedButton(
+                        //   onPressed: () => _save(),
+                        // ),
+                        RaisedButton(
+                          onPressed: () async {
+                            await _save(context);
+                            // return showDialog(
+                            //   context: context,
+                            //   builder: (BuildContext context) {
+                            //     return Dialog(
+                            //       shape: RoundedRectangleBorder(
+                            //           borderRadius: BorderRadius.circular(20.0)),
+                            //       child: Container(
+                            //         width: 800,
+                            //         height: 800,
+                            //         child: Padding(
+                            //           padding: const EdgeInsets.all(12.0),
+                            //           child: MicrotaskScreen(mission.missionId),
+                            //         ),
+                            //       ),
+                            //     );
+                            //   },
+                            // );
+                          },
+                          child: Text('Create Mission AND add microtask'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
     );
   }
 }
--- lib/screens/microtasks.dart
+++ lib/screens/microtasks.dart
@@ -4,140 +4,179 @@ import 'package:forms/models/providers/microtask_provider.dart';
 import 'package:forms/models/providers/mission_provider.dart';
 import 'package:provider/provider.dart';
 
-class MicrotaskScreen extends StatelessWidget {
+class MicrotaskScreen extends StatefulWidget {
   static const routeName = '/microtask';
-  //MicrotaskScreen(this.id);
+
+  @override
+  _MicrotaskScreenState createState() => _MicrotaskScreenState();
+}
+
+class _MicrotaskScreenState extends State<MicrotaskScreen> {
+  bool isLoading = false;
+
   var micro = Microtask();
+
   final _formKey = GlobalKey<FormState>();
-  Future<void> _save(BuildContext context,String id) async {
-      _formKey.currentState.save();
-    await Provider.of<MicroTaskProvider>(context,listen: false).createMicrotask(micro, id);
-    _formKey.currentState.reset();
+
+  Future<void> _save(BuildContext context, String id) async {
+    _formKey.currentState.save();
+    setState(() {
+      isLoading = true;
+    });
+    await Provider.of<MicroTaskProvider>(context, listen: false)
+        .createMicrotask(micro, id);
+    // setState(() {
+    //   isLoading = false;
+    // });
   }
 
   @override
   Widget build(BuildContext context) {
-    final String id=Provider.of<MissionProvider>(context,listen: false).id;
+    final String id = Provider.of<MissionProvider>(context, listen: false).id;
     return Scaffold(
-      body: Container(
-        width: 500,
-        height: 500,
-        padding: const EdgeInsets.all(15),
-        child: Form(
-          key: _formKey,
-          child: ListView(
-            children: <Widget>[
-              TextFormField(
-                decoration: InputDecoration(labelText: "answer"),
-                textInputAction: TextInputAction.next,
-                onSaved: (value) {
-                  micro = Microtask(
-                    answer: value,
-                    detail: micro.detail,
-                    name: micro.name,
-                    progress: micro.progress,
-                    question: micro.question,
-                    resources: micro.resources,
-                  );
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "details"),
-                textInputAction: TextInputAction.next,
-                onSaved: (value) {
-                  micro = Microtask(
-                    answer: micro.answer,
-                    detail: value,
-                    name: micro.name,
-                    progress: micro.progress,
-                    question: micro.question,
-                    resources: micro.resources,
-                  );
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "name"),
-                textInputAction: TextInputAction.next,
-                onSaved: (value) {
-                  micro = Microtask(
-                    answer: micro.answer,
-                    detail: micro.detail,
-                    name: value,
-                    progress: micro.progress,
-                    question: micro.question,
-                    resources: micro.resources,
-                  );
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "progress"),
-                textInputAction: TextInputAction.next,
-                onSaved: (value) {
-                  micro = Microtask(
-                    answer: micro.answer,
-                    detail: micro.detail,
-                    name: micro.name,
-                    progress: int.parse(value),
-                    question: micro.question,
-                    resources: micro.resources,
-                  );
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "Name"),
-                textInputAction: TextInputAction.next,
-                onSaved: (value) {
-                  micro = Microtask(
-                    answer: micro.answer,
-                    detail: micro.detail,
-                    name: micro.name,
-                    progress: micro.progress,
-                    question: value,
-                    resources: micro.resources,
-                  );
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "resources"),
-                textInputAction: TextInputAction.next,
-                onSaved: (value) {
-                  var lt = new List<String>();
-                  final _delimiter = ',';
-                  final _values = value.split(_delimiter);
-                  //print(_values);
-                  _values.forEach((item) {
-                    lt.add(item);
-                  });
-                  micro = Microtask(
-                    answer: micro.answer,
-                    detail: micro.detail,
-                    name: micro.name,
-                    progress: micro.progress,
-                    question: micro.question,
-                    resources: lt,
-                  );
-                },
-              ),
-              Row(
-                children: [
-                  RaisedButton(
-                    child: Text("ADD Another microtask"),
-                    onPressed: () async => await _save(context,id),
-                  ),
-                  SizedBox(width: 20,),
-                  RaisedButton(
-                    child: Text("DONE!!"),
-                    onPressed: () async {
-                      await _save(context,id);
-                      Navigator.of(context).pop();
-                    },
-                  )
-                ],
-              ),
-            ],
-          ),
-        ),
+      appBar: AppBar(
+        leading: Container(),
       ),
+      body: isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Center(
+              child: Card(
+                elevation: 10,
+                child: Container(
+                  alignment: Alignment.center,
+                  width: 500,
+                  height: 500,
+                  padding: const EdgeInsets.all(15),
+                  child: Form(
+                    key: _formKey,
+                    child: ListView(
+                      children: <Widget>[
+                        TextFormField(
+                          decoration: InputDecoration(labelText: "Answer"),
+                          textInputAction: TextInputAction.next,
+                          onSaved: (value) {
+                            micro = Microtask(
+                              answer: value,
+                              detail: micro.detail,
+                              name: micro.name,
+                              progress: micro.progress,
+                              question: micro.question,
+                              resources: micro.resources,
+                            );
+                          },
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(labelText: "Details"),
+                          textInputAction: TextInputAction.next,
+                          onSaved: (value) {
+                            micro = Microtask(
+                              answer: micro.answer,
+                              detail: value,
+                              name: micro.name,
+                              progress: micro.progress,
+                              question: micro.question,
+                              resources: micro.resources,
+                            );
+                          },
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(labelText: "Name"),
+                          textInputAction: TextInputAction.next,
+                          onSaved: (value) {
+                            micro = Microtask(
+                              answer: micro.answer,
+                              detail: micro.detail,
+                              name: value,
+                              progress: micro.progress,
+                              question: micro.question,
+                              resources: micro.resources,
+                            );
+                          },
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(labelText: "Progress"),
+                          textInputAction: TextInputAction.next,
+                          onSaved: (value) {
+                            micro = Microtask(
+                              answer: micro.answer,
+                              detail: micro.detail,
+                              name: micro.name,
+                              progress: int.parse(value),
+                              question: micro.question,
+                              resources: micro.resources,
+                            );
+                          },
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(labelText: "Question"),
+                          textInputAction: TextInputAction.next,
+                          onSaved: (value) {
+                            micro = Microtask(
+                              answer: micro.answer,
+                              detail: micro.detail,
+                              name: micro.name,
+                              progress: micro.progress,
+                              question: value,
+                              resources: micro.resources,
+                            );
+                          },
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(labelText: "Resources"),
+                          textInputAction: TextInputAction.next,
+                          onSaved: (value) {
+                            var lt = new List<String>();
+                            final _delimiter = ',';
+                            final _values = value.split(_delimiter);
+                            //print(_values);
+                            _values.forEach((item) {
+                              lt.add(item);
+                            });
+                            micro = Microtask(
+                              answer: micro.answer,
+                              detail: micro.detail,
+                              name: micro.name,
+                              progress: micro.progress,
+                              question: micro.question,
+                              resources: lt,
+                            );
+                          },
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+                            RaisedButton(
+                              child: Text("ADD Another microtask"),
+                              onPressed: () async {
+                                await _save(context, id);
+                                setState(() {
+                                  isLoading=false;
+                                });
+                                //_formKey.currentState.reset();
+                              },
+                            ),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            RaisedButton(
+                              child: Text("DONE!!"),
+                              onPressed: () async {
+                                await _save(context, id);
+                                Navigator.of(context).pushReplacementNamed('/');
+                              },
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
     );
   }
 }
